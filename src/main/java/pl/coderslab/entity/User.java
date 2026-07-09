53db1993a336f86d5ec3b8247477139400959a71
lukoc1 <lukoc@visa.com>
2026-07-09 16:03:15 +0200

    User class and UserDao class added

---
+++ src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,45 @@
+package pl.coderslab.entity;
+
+public class User {
+
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public User setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
